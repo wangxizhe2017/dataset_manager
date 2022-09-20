f5e89b07ee612455e4898c73b24c98572afbad1e
wangxizhe2017 <wangxizhe_edu@163.com>
2022-09-20 16:47:48 -0400

    pre_0.0.0.2
    pre_0.0.0.2

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,4 +1,4 @@
-from basic.bbox import BBox
-from basic.bbox_list import BBoxList
+from bbox import BBox
+from bbox_list import BBoxList
 from image_dict import ImageDict
 from dataset_core import DatasetCore
